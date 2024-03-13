885cadb2ff535464f6af0ad700974f421d48afc3
Mixail132 <millionbaxov@gmail.com>
2024-03-13 21:00:55 +0300

    Get the very first answers from GPT
    Make a series of requests and get the answers. But it works too slow.

--- databasechecker/views.py
+++ databasechecker/views.py
@@ -1,6 +1,30 @@
 from connections import cursor
+import requests
+import time
 
-request = """ SELECT body FROM backend """
-cursor.execute(request)
-for i in cursor.fetchall():
-    print(i)
+prompt = """ 
+Прочитай текст вакансии и определи относится ли он к backend разработке? 
+Ответь пожалуйста да или нет?
+"""
+
+
+def make_sql():
+    request = """ SELECT body FROM backend LIMIT 10"""
+    cursor.execute(request)
+    return cursor.fetchall()
+
+
+def ask_gpt(vacancy):
+    url = "https://mixail132.pythonanywhere.com/askgpt"
+    data = {"question": f"{prompt} Текст вакансии: '{vacancy}'"}
+    response = requests.post(url=url, json=data)
+    answer = response.json()
+    # print(answer)
+    return answer['answer']
+
+
+if __name__ == "__main__":
+    all_vacancies = make_sql()
+    for vacancy_text in all_vacancies:
+        time.sleep(10)
+        print((ask_gpt(vacancy_text[0])))
